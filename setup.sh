160c73a407d865eda11990df214b7e0b4413e12e
Romesh <57130659+romeshb@users.noreply.github.com>
2021-12-29 02:26:20 +0530

    checking by removing the following code
    echo "\
    [server]\n\
    headless = true\n\
    enableCORS=false\n\
    port = $PORT\n\
    " > ~/.streamlit/config.toml

--- setup.sh
+++ setup.sh
@@ -5,9 +5,3 @@ echo "\
 email = \"email@domain\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
