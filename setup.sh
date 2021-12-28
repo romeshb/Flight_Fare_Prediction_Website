1b47160ab25bb90c7aa39889d128e62e38a52214
Romesh <57130659+romeshb@users.noreply.github.com>
2021-12-29 03:10:43 +0530

    changed to custom theame
    #working code in comments
    mkdir -p ~/.streamlit/
    
    echo "\
    [general]\n\
    email = \"your-email@domain.com\"\n\
    " > ~/.streamlit/credentials.toml
    
    echo "\
    [server]\n\
    headless = true\n\
    enableCORS=false\n\
    port = $PORT\n\
    " > ~/.streamlit/config.toml

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]\n\
+base='light'\n\
+primaryColor='#3a70ec'\n\
+backgroundColor='#FFD600'\n\
+secondaryBackgroundColor = ‘#F0F2F6’\n\
+textColor='#0e1862'\n\
+
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
