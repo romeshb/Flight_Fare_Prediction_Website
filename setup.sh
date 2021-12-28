aab6d64a974d1281d121595360ccb58e562acd70
Romesh <57130659+romeshb@users.noreply.github.com>
2021-12-29 02:41:13 +0530

    added them config
    [theme]
    base="light"
    primaryColor="#3a70ec"
    backgroundColor="#ffd600"
    textColor="#0e1862"

--- setup.sh
+++ setup.sh
@@ -5,3 +5,17 @@ echo "\
 email = \"email@domain\"\n\
 " > ~/.streamlit/credentials.toml
 
+echo "[theme]
+base='light'
+primaryColor='#3a70ec'
+backgroundColor='#FFD600'
+textColor='#0e1862'
+secondaryBackgroundColor=’#F0F2F6’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
+
+
